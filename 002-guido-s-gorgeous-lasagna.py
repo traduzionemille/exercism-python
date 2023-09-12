5981d0290629de365c4d5d2afa45457b38a58c69
Paola Ruffo <120667980+traduzionemille@users.noreply.github.com>
2023-09-12 19:43:47 +0100

    Update 002-guido-s-gorgeous-lasagna.py

--- 002-guido-s-gorgeous-lasagna.py
+++ 002-guido-s-gorgeous-lasagna.py
@@ -1,17 +1,3 @@
-"""Functions used in preparing Guido's gorgeous lasagna.
-
-Learn about Guido, the creator of the Python language:
-https://en.wikipedia.org/wiki/Guido_van_Rossum
-
-This is a module docstring, used to describe the functionality
-of a module and its functions and/or classes.
-"""
-
-
-#TODO: define the 'EXPECTED_BAKE_TIME' constant.
-EXPECTED_BAKE_TIME = 40
-
-#TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
@@ -28,8 +14,6 @@ def bake_time_remaining(elapsed_bake_time):
     return remaining_time
 
 
-#TODO: Define the 'preparation_time_in_minutes()' function below.
-# You might also consider using 'PREPARATION_TIME' here, if you have it defined.
 def preparation_time_in_minutes(number_of_layers):
     """Calculate the lasagna preparation time
     
@@ -42,8 +26,6 @@ def preparation_time_in_minutes(number_of_layers):
     return prep_time_in_min
 
     
-#TODO: define the 'elapsed_time_in_minutes()' function below.
-# Remember to add a docstring (you can copy and then alter the one from bake_time_remaining.)
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     """Calculate the sum of preparation time and elapsed bake time
     
